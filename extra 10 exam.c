b36be196e6bad32785b164159de7f799f63471cb
jenali0829 <120018077+jenali0829@users.noreply.github.com>
2023-01-20 17:11:21 +0530

    Add files via upload

---
+++ extra 10 exam.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+main(){
+	
+	int r,c,k;
+	
+	for(r=5; r>=1; r--){
+		
+		for(k=r; k<=1; k++){
+			 printf(" ");
+			 
+			for(c=1; c<=r; c++){
+				
+				printf("%d",c);
+			}
+			
+			printf("\t");
+		}
+
+			
+			
+		}
+		
+	
+	for(r=5; r>=1; r--){
+		
+		for(c=1; c<=r; c++){
+			
+			printf("%d",c);
+		}
+		printf("\n");
+	}
+}
+	
+	
